898abf0616e4d7ced15976a2c60235cb7e57f28a
felipe <fae.20@hotmail.com>
2023-11-19 00:04:33 -0300

    fix: javadocs

---
+++ sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,11 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // olá, sou um comentario
+
+        /*ola sou um 
+        comentario multilinhas  */
+
+        //* */
+    }
+    
+}
